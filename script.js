807a0dea3888b8caa29c6efac17983e1158175f0
Samuel <samuel210@live.fr>
2021-05-03 21:26:29 +0200

    Correction bug et reste un autre bu a corriger

--- script.js
+++ script.js
@@ -1,94 +1,91 @@
 class Calculatrice {
-    constructor(operationPrecedenteTextElement, operationEncourTextElement) {
-        this.operationPrecedenteTextElement = operationPrecedenteTextElement;
-        this.operationEncourTextElement = operationEncourTextElement;
-        this.effacer();
-    }
+  constructor(operationPrecedenteTextElement, operationEncourTextElement) {
+    this.operationPrecedenteTextElement = operationPrecedenteTextElement;
+    this.operationEncourTextElement = operationEncourTextElement;
+    this.effacer();
+  }
 
-    effacer() {
-        this.operateurEnCour = "";
-        this.operateurPrecedent = "";
-        this.operation = undefined;
-    }
+  effacer() {
+    this.operateurEnCour = "";
+    this.operateurPrecedent = "";
+    this.operation = undefined;
+  }
 
-    supprimer() {
-        this.operateurEnCour = this.operateurEnCour.toString().slice(0, -1);
-    }
+  supprimer() {
+    this.operateurEnCour = this.operateurEnCour.toString().slice(0, -1);
+  }
 
-    ajouterNombre(nombre) {
-        if (nombre === "." && this.operateurEnCour.includes(".")) return;
+  ajouterNombre(nombre) {
+    if (nombre === "." && this.operateurEnCour.includes(".")) return;
 
-        this.operateurEnCour = this.operateurEnCour.toString() + nombre.toString();
-        if (this.operation === undefined) {
-            this.operateurEnCour = nombre.toString();
-        }
-    }
+    this.operateurEnCour = this.operateurEnCour.toString() + nombre.toString();
+  }
 
-    choixOperation(operation) {
-        if (this.operateurEnCour === "") return;
-        if (this.operateurPrecedent !== "") {
-            this.compute();
-        }
-        this.operation = operation;
-        this.operateurPrecedent = this.operateurEnCour;
-        this.operateurEnCour = "";
+  choixOperation(operation) {
+    if (this.operateurEnCour === "") return;
+    if (this.operateurPrecedent !== "") {
+      this.compute();
     }
+    this.operation = operation;
+    this.operateurPrecedent = this.operateurEnCour;
+    this.operateurEnCour = "";
+  }
 
-    compute() {
-        let validation;
-        const avant = parseFloat(this.operateurPrecedent);
-        const enCours = parseFloat(this.operateurEnCour);
-        if (isNaN(avant) || isNaN(enCours)) return;
-        switch (this.operation) {
-            case "+":
-                validation = avant + enCours;
-                break;
-            case "-":
-                validation = avant - enCours;
-                break;
-            case "/":
-                validation = avant / enCours;
-                break;
-            case "*":
-                validation = avant * enCours;
-                break;
-            default:
-                return;
-        }
-        this.operateurEnCour = validation;
-        this.operation = undefined;
-        this.operateurPrecedent = "";
+  compute() {
+    let validation;
+    const avant = parseFloat(this.operateurPrecedent);
+    const enCours = parseFloat(this.operateurEnCour);
+    if (isNaN(avant) || isNaN(enCours)) return;
+    switch (this.operation) {
+      case "+":
+        validation = avant + enCours;
+        break;
+      case "-":
+        validation = avant - enCours;
+        break;
+      case "/":
+        validation = avant / enCours;
+        break;
+      case "*":
+        validation = avant * enCours;
+        break;
+      default:
+        return;
     }
-    getEcranNombre(nombre) {
-        const StringNombre = nombre.toString();
-        const integerDigits = parseFloat(StringNombre.split(".")[0]);
-        const decimalDigits = StringNombre.split(".")[1];
-        let intergerEcran;
-        if (isNaN(integerDigits)) {
-            intergerEcran = "";
-        } else {
-            intergerEcran = integerDigits.toLocaleString("fr", {
-                maximumFractionDigits: 0,
-            });
-        }
-        if (decimalDigits != null) {
-            return `${intergerEcran}.${decimalDigits}`;
-        } else {
-            return intergerEcran;
-        }
+    this.operateurEnCour = validation;
+    this.operation = undefined;
+    this.operateurPrecedent = "";
+  }
+  getEcranNombre(nombre) {
+    const StringNombre = nombre.toString();
+    const integerDigits = parseFloat(StringNombre.split(".")[0]);
+    const decimalDigits = StringNombre.split(".")[1];
+    let intergerEcran;
+    if (isNaN(integerDigits)) {
+      intergerEcran = "";
+    } else {
+      intergerEcran = integerDigits.toLocaleString("fr", {
+        maximumFractionDigits: 0,
+      });
     }
-    updateEcran() {
-        this.operationEncourTextElement.innerText = this.getEcranNombre(
-            this.operateurEnCour
-        );
-        if (this.operation != null) {
-            this.operationPrecedenteTextElement.innerText = `${this.getEcranNombre(
+    if (decimalDigits != null) {
+      return `${intergerEcran}.${decimalDigits}`;
+    } else {
+      return intergerEcran;
+    }
+  }
+  updateEcran() {
+    this.operationEncourTextElement.innerText = this.getEcranNombre(
+      this.operateurEnCour
+    );
+    if (this.operation != null) {
+      this.operationPrecedenteTextElement.innerText = `${this.getEcranNombre(
         this.operateurPrecedent
       )} ${this.operation}`;
-        } else {
-            this.operationPrecedenteTextElement.innerText = "";
-        }
+    } else {
+      this.operationPrecedenteTextElement.innerText = "";
     }
+  }
 }
 
 const nombreBoutons = document.querySelectorAll("[data-number]");
@@ -97,42 +94,42 @@ const egalBouton = document.querySelector("[data-equals]");
 const supprimerBouton = document.querySelector("[data-delete]");
 const effacerTousBouton = document.querySelector("[data-all-clear]");
 const operationPrecedenteTextElement = document.querySelector(
-    "[data-previous-operand]"
+  "[data-previous-operand]"
 );
 const operationEncourTextElement = document.querySelector(
-    "[data-current-operand]"
+  "[data-current-operand]"
 );
 
 const calculatrice = new Calculatrice(
-    operationPrecedenteTextElement,
-    operationEncourTextElement
+  operationPrecedenteTextElement,
+  operationEncourTextElement
 );
 
 nombreBoutons.forEach((bouton) => {
-    bouton.addEventListener("click", () => {
-        calculatrice.ajouterNombre(bouton.innerText);
-        calculatrice.updateEcran();
-    });
+  bouton.addEventListener("click", () => {
+    calculatrice.ajouterNombre(bouton.innerText);
+    calculatrice.updateEcran();
+  });
 });
 
 operationBoutons.forEach((bouton) => {
-    bouton.addEventListener("click", () => {
-        calculatrice.choixOperation(bouton.innerText);
-        calculatrice.updateEcran();
-    });
+  bouton.addEventListener("click", () => {
+    calculatrice.choixOperation(bouton.innerText);
+    calculatrice.updateEcran();
+  });
 });
 
 egalBouton.addEventListener("click", (bouton) => {
-    calculatrice.compute();
-    calculatrice.updateEcran();
+  calculatrice.compute();
+  calculatrice.updateEcran();
 });
 
 effacerTousBouton.addEventListener("click", (bouton) => {
-    calculatrice.effacer();
-    calculatrice.updateEcran();
+  calculatrice.effacer();
+  calculatrice.updateEcran();
 });
 
 supprimerBouton.addEventListener("click", (e) => {
-    calculatrice.supprimer();
-    calculatrice.updateEcran();
+  calculatrice.supprimer();
+  calculatrice.updateEcran();
 });
